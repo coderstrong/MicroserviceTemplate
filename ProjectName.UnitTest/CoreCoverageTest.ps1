95503ef3b0e563287d255a6b872a11c78d54d1a9
coderstrong <thao31b@gmail.com>
2020-01-08 00:37:01 +0700

    Add PowerShell check unit test coverage

---
+++ ProjectName.UnitTest/CoreCoverageTest.ps1
@@ -0,0 +1,30 @@
+$testProjectPath = $PSScriptRoot
+$testResultsFolder = "$PSScriptRoot\TestResults"
+
+<#
+echo "Test Project Path" $testProjectPath
+echo "Test Results Folder" $testResultsFolder
+#>
+
+try {
+    #$testSettingsPath = Get-ChildItem -File -Filter *.runsettings -Path $testProjectPath -Name -Recurse | Select-Object -First 1;
+    #dotnet test $testProjectPath --settings:$testSettingsPath --collect:"Code Coverage"
+	dotnet test $testProjectPath --collect:"Code Coverage"
+    $recentCoverageFile = Get-ChildItem -File -Filter *.coverage -Path $testResultsFolder -Name -Recurse | Select-Object -First 1;
+    write-host 'Test Completed'  -ForegroundColor Green
+
+    C:\Users\coder\.nuget\packages\microsoft.codecoverage\16.2.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze  /output:$testResultsFolder\MyTestOutput.coveragexml  $testResultsFolder'\'$recentCoverageFile
+    write-host 'CoverageXML Generated'  -ForegroundColor Green
+
+    reportgenerator "-reports:$testResultsFolder\MyTestOutput.coveragexml" "-targetdir:$testResultsFolder\coveragereport"
+    write-host 'CoverageReport Published'  -ForegroundColor Green
+
+}
+catch {
+
+    write-host "Caught an exception:" -ForegroundColor Red
+    write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+    write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
+}
+
+Read-Host -Prompt "Press Enter to continue"
